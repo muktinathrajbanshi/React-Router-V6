f1f95393924395f2caeccea9889da9bab682ebc3
unknown <muktinathrajbanshi786@gmail.com>
2025-10-02 23:09:44 +0545

    just structure is created for header section

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,3 +1,52 @@
 export const Header = () => {
-    return <h1>Header</h1>;
+    return (
+        <>
+            <header className="section-navbar">
+                <section className="top_txt">
+                    <div className="head container">
+                        <div className="head_txt">
+                            <p>Get Mukti Membership, 30-day return or refund guarantee.</p>
+                        </div>
+                        <div className="sing_in_up">
+                            <a href="#">SIGN IN</a>
+                            <a href="#">SIGN UP</a>
+                        </div>
+                    </div>
+                </section>
+
+                <div className="container">
+                    <div className="navbar-brand">
+                        <a href="index">
+                            <p>MuktiFlix</p>
+                        </a>
+                    </div>
+
+                    <nav className="navbar">
+                        <ul>
+                            <li className="nav-item">
+                                <a href="/">
+                                    Home
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="/about">
+                                    about
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="/movie">
+                                    movies
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="/contact">
+                                    contact
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+        </>
+    );
 };
