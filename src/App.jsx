df0748ce1a589adea013d4839bdba92aaab19f8f
unknown <muktinathrajbanshi786@gmail.com>
2025-10-02 22:42:28 +0545

    adding header and footer with unique react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,50 @@
 import React from 'react';
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Movie } from './pages/Movie';
 import { Contact } from './pages/Contact';
+import AppLayout from './components/layout/AppLayout';
 
 export const App = () => {
 
-  // const router = createBrowserRouter([
-  //   {
-  //     path: "/",
-  //     element:<Home />
-  //   },
-  //   {
-  //     path: "/about",
-  //     element:<About />
-  //   },
-  //   {
-  //     path: "/movie",
-  //     element:<Movie />
-  //   },
-  //   {
-  //     path: "/contact",
-  //     element:<Contact />
-  //   }
-  // ]);
+  const router = createBrowserRouter([
+
+    {
+      path:"/",
+      element:<AppLayout />
+    },
+
+    {
+      path: "/",
+      element:<Home />
+    },
+    {
+      path: "/about",
+      element:<About />
+    },
+    {
+      path: "/movie",
+      element:<Movie />
+    },
+    {
+      path: "/contact",
+      element:<Contact />
+    }
+  ]);
 
 
-    const router = createBrowserRouter(
-      createRoutesFromElements(
-        <Route>
-          <Route path = "/" element={<Home />} />
-          <Route path = "/about" element={<About />} />
-          <Route path = "/movie" element={<Movie />} />
-          <Route path = "/contact" element={<Contact />} />                   
-        </Route>
-      )
-    );
+    // const router = createBrowserRouter(
+    //   createRoutesFromElements(
+    //     <Route>
+    //       <Route path = "/" element={<Home />} />
+    //       <Route path = "/about" element={<About />} />
+    //       <Route path = "/movie" element={<Movie />} />
+    //       <Route path = "/contact" element={<Contact />} />                   
+    //     </Route>
+    //   )
+    // );
 
   return <RouterProvider router={router} />;
 };
---
+++ src/components/layout/AppLayout.jsx
@@ -0,0 +1,13 @@
+import { Footer } from "./Footer";
+import { Header } from "./Header";
+
+const AppLayout = () => {
+    return (
+        <>
+        <Header />
+        <Footer />
+    </>
+    ); 
+};
+
+export default AppLayout;
