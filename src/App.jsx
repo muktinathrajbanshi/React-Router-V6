cb569d030bb07ffe3223c4af55e4a2c3bf9b569f
unknown <muktinathrajbanshi786@gmail.com>
2025-10-02 17:53:05 +0545

    pages is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { createBrowserRouter } from "react-router-dom";
 import "./App.css";
 
 export const App = () => {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      
+    }
+  ]);
+
+
   return (
     <>
+
       <h1>Happy Dashin, Tihar, Chhat, Ekadashi!</h1>
     </>
   );
