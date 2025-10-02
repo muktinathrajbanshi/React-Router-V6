5fc2db408e109374a7bfabac9422a6d324411c55
unknown <muktinathrajbanshi786@gmail.com>
2025-10-02 19:38:10 +0545

    React Router v6.4: Simplify Route Setup with createRoutesFromElements Helper Function

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 import "./App.css";
 import { Home } from './pages/Home';
 import { About } from './pages/About';
@@ -8,25 +8,36 @@ import { Contact } from './pages/Contact';
 
 export const App = () => {
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element:<Home />
-    },
-    {
-      path: "/about",
-      element:<About />
-    },
-    {
-      path: "/movie",
-      element:<Movie />
-    },
-    {
-      path: "/contact",
-      element:<Contact />
-    }
-  ]);
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element:<Home />
+  //   },
+  //   {
+  //     path: "/about",
+  //     element:<About />
+  //   },
+  //   {
+  //     path: "/movie",
+  //     element:<Movie />
+  //   },
+  //   {
+  //     path: "/contact",
+  //     element:<Contact />
+  //   }
+  // ]);
 
+
+    const router = createBrowserRouter(
+      createRoutesFromElements(
+        <Route>
+          <Route path = "/" element={<Home />} />
+          <Route path = "/about" element={<About />} />
+          <Route path = "/movie" element={<Movie />} />
+          <Route path = "/contact" element={<Contact />} />                   
+        </Route>
+      )
+    );
 
   return <RouterProvider router={router} />;
 };
