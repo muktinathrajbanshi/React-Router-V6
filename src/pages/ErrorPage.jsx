13ac897a95188e2babcdc60c33894d05df16ea11
unknown <muktinathrajbanshi786@gmail.com>
2025-10-04 09:35:10 +0545

    404 error page structure is just created

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,3 +1,30 @@
+import { useRouteError } from "react-router-dom";
+
 export const ErrorPage = () => {
+
+    const error = useRouteError();
+    // console.log(error);
+
+    if(error.status === 404) {
+        return (
+            <section className="error-section">
+             <div className="error-text">
+                <figure>
+                    <img src="https://cdn.dribbble.com/users/722246/screenshots/3066818/404-page.gif"
+                     alt="404 page" 
+                     />
+                </figure>
+                <div className="text-center">
+                    <p className="p-a">
+                        . The page you were looking for could not be found
+                    </p>
+                    <p className="p-b">... Back to previous page</p>
+                </div>
+                </div>
+            </section>
+        );
+    }
+    
+
     return <h1>404 error page</h1>;
 };
