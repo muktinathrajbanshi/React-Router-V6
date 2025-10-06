0a6449872411ef871b31457f3652c8d55f3052c3
unknown <muktinathrajbanshi786@gmail.com>
2025-10-06 08:05:01 +0545

    creating component as function an and calling back to app.jsx

--- src/api/GetAPIData.jsx
+++ src/api/GetAPIData.jsx
@@ -1,10 +1,12 @@
-export const GetAPIData = async() => {
+export const getMoviesData = async() => {
     try {
-        fetch(
+     const response = await fetch(
             "https://www.omdbapi.com/?i=tt3896198&apikey=1c12799f&s=titanic&page=1"
         );
+        const data = response.json();
+        return data;
     } catch (error) {
         console.log(error);
         
     }
-}
+};
