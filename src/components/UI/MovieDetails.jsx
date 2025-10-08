62223942eaa0f984425c2958d58ffbbc3e3165e1
unknown <muktinathrajbanshi786@gmail.com>
2025-10-08 21:51:34 +0545

    destructing every element from API

--- src/components/UI/MovieDetails.jsx
+++ src/components/UI/MovieDetails.jsx
@@ -1,14 +1,27 @@
 import { useLoaderData } from "react-router-dom";
+import "../UI/Card.css";
 
 export const MovieDetails = () => {
-
-    // const params = useParams();
-    // console.log(params);
-    
-
     const movieData = useLoaderData();
     console.log(movieData);
     
+    const { Actor, Poster, Title, Type, Year, Plot, BoxOffice, imdbID } = movieData;
 
-    return <h1>Hello Movie {params.movieID} </h1>;
+    return (
+        <li className="hero-container hero-movie-container">
+            <div className="main-container">
+                <div className="poster-container">
+                    <img src={Poster} className="poster" alt={Title} />
+                </div>
+                <div className="ticket-container">
+                    <div className="ticket__content">
+                        <h4 className="ticket__movie-title"></h4>
+                        <p className="ticket__current-price">Type: </p>
+                        <p className="ticket__current-price">Type: </p>
+                        <button className="ticket__buy-btn">Watch now</button>
+                    </div>
+                </div>
+            </div>
+        </li>
+    )
 };
