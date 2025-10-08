5f45fe1323548f9bb87573ad485ddf36adee6f24
unknown <muktinathrajbanshi786@gmail.com>
2025-10-08 20:29:58 +0545

    still facing same problem

---
+++ src/components/UI/Card.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+export const Card = ({ curMovie }) => {
+
+  return (
+    <>
+      <li className="hero-container">
+        <div className="main-container">
+            <div className="poster-container">
+                <img src={Poster} className="poster" alt={imdbID} />
+            </div>
+            <div className="ticket-container">
+                <div className="ticket__content">
+                    <button className="ticket__buy-btn">Watch now</button>
+                </div>
+            </div>
+        </div>
+      </li>
+    </>
+  );
+};
