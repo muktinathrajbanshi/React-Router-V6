a4ab66dd1cf253c1acae79e865354c6246dd4128
unknown <muktinathrajbanshi786@gmail.com>
2025-10-09 16:23:32 +0545

    just now i am going to look for contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,16 @@
 export const Contact = () => {
-    return <h1>Contact Page</h1>;
+    return (
+        <>
+            <section className="container section-contact">
+                <div className="container">
+                    <h2 className="section-common--heading">contact us</h2>
+                    <p>
+                        Get in touch with us. We are always here to help you.
+                    </p>
+                </div>
+
+                
+            </section>
+        </>
+    )
 };
