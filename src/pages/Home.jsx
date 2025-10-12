118a8446b104dec55242a4de825c3dc153275d4e
unknown <muktinathrajbanshi786@gmail.com>
2025-10-12 20:07:33 +0545

    70% structured completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 export const Home = () => {
   return (
@@ -9,6 +10,18 @@ export const Home = () => {
             <p className="hero-subheading">
               Explore the Latest in Movie Industries
             </p>
+            <h1 className="hero-heading">
+              Unlimited Movie, TVs Shows, & More.
+            </h1>
+            <p className="hero-para">
+              Discover the Top Best Movies and Dramas with a catchy subtitle
+              like Your Ultimate Guide to Must-Watch Content.
+            </p>
+            <div className="hero-btn">
+              <NavLink to="products.html" className="btn">
+                Explore Now
+              </NavLink>
+            </div>
           </div>
         </div>
       </div>
