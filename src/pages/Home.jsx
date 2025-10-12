ee0dcb4d0de6ea46aa5e1f4ebef4b893c04aa2c7
unknown <muktinathrajbanshi786@gmail.com>
2025-10-12 20:02:08 +0545

    haha just structuring home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,17 @@
 import React from 'react'
 
 export const Home = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <main>
+      <div className="section-hero">
+        <div className="container grid grid-two--cols">
+          <div className="section-hero--content">
+            <p className="hero-subheading">
+              Explore the Latest in Movie Industries
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
