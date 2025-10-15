835d6e896e30a52bd51a92355bb60ade96916a21
unknown <muktinathrajbanshi786@gmail.com>
2025-10-15 21:18:00 +0545

    halka changes

--- src/components/UI/MovieDetails.jsx
+++ src/components/UI/MovieDetails.jsx
@@ -12,7 +12,19 @@ export const MovieDetails = () => {
             <div className="main-container">
                <figure className="movie">
                  <div className="movie__hero">
-                    <img src={Poster} alt="Rambo" />
+                    <img src={Poster} alt="Rambo" className="movie__img" />
+                 </div>
+                 <div className="movie__content">
+                    <div className="movie__title">
+                        <h1 className="heading__primary">
+                            {Title} <i className="fas fa-fire"></i>
+                        </h1>
+                        <div className="movie__tag movie__tag--1">{`#${Type}`}</div>
+                        <div className="movie__tag movie__tag--2">Year: {Year}</div>
+                    </div>
+                    <div className="movie__description">{Plot}</div>
+                    <br />
+                    <p className="movie__description">Awards: {Awards}</p>
                  </div>
                </figure>
             </div>
