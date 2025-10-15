b7e09d0b5c39ed01f231fbad74cffda8073a6e90
unknown <muktinathrajbanshi786@gmail.com>
2025-10-15 19:36:39 +0545

    yesterday i did little bit mistake now today i will complete it

--- src/components/UI/MovieDetails.jsx
+++ src/components/UI/MovieDetails.jsx
@@ -10,60 +10,11 @@ export const MovieDetails = () => {
     return (
         <li className="hero-container hero-movie-container">
             <div className="main-container">
-
-                <figure className="movie">
-                    <div className="movie__hero">
-                        <img src={Poster} alt="Rambo" className="movie_img" />
-                    </div>
-                    <div className="movie__content">
-                        <div className="movie__title">
-                            <h1>
-                                {Title} <i className="fas fa-fire"></i>
-                            </h1>
-                            <div className="movie__tag movie__tag--1">{`#${Type}`}</div>
-                            <div className="movie__tag movie__tag--2">Year: {Year}</div>
-                        </div>
-                        <div className="movie__description">{Plot}</div>
-                        <br />
-                        <p className="movie__description">Awards: {Awards}</p>
-                        <div className="movie__details">
-                            <p className="movie__detail">
-                                <span className="icons icons-red">
-                                    <i className="fas fa-camera-retro"></i>
-                                </span>
-                                Rating: {imdbRating}
-                            </p>
-                            <p className="movie__detail">
-                                <span className="icons icons-grey">
-                                    <i className="fas fa-clock"></i>
-                                </span>
-                                {formattedTime}
-                            </p>
-                            <p className="movie__detail">
-                                <span className="icons icons-yellow">
-                                    <i className="fas fa-file-invoice-dollar"></i>
-                                </span>
-                                {BoxOffice}
-                            </p>
-                        </div>
-                    </div>
-                    <NavLink
-                    to="/movie"
-                    className="movie__tag movie__tag--2"
-                    >
-
-                    </NavLink>
-                </figure>
-                
-                <div className="ticket-container">
-                    <div className="ticket__content">
-                        <h4 className="ticket__movie-title">{Title}</h4>
-                        <p>{Plot}</p>
-                        <p className="ticket__current-price">Type: </p>
-                        <p className="ticket__current-price">Type: </p>
-                        <button className="ticket__buy-btn">Watch now</button>
-                    </div>
-                </div>
+               <figure className="movie">
+                 <div className="movie__hero">
+                    <img src={Poster} alt="Rambo" />
+                 </div>
+               </figure>
             </div>
         </li>
     )
