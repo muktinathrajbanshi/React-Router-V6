d5c3d27915bb49f9c0e10a2aa1b822ece39c1f9c
unknown <muktinathrajbanshi786@gmail.com>
2025-10-16 21:06:00 +0545

    now it's totally completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import "../App.css";
 
 export const Home = () => {
   return (
+    <>
     <main>
       <div className="section-hero">
         <div className="container grid grid-two--cols">
@@ -33,14 +34,17 @@ export const Home = () => {
             height="150"
             />
           </div>
-        </div>
-      </div>
 
-      <div class="custom-shape-divider-top-1760627476">
-        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-            <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
-        </svg>
+          
+        </div>
+        
       </div>
     </main>
+        <div class="custom-shape-divider-bottom-1760627935">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+              <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+          </svg>
+        </div>
+    </>
   );
 };
